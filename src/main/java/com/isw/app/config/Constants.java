5318446bf79061c1e0549b4058f89add437be494
Cris T <crisltejerina@gmail.com>
2021-06-20 12:41:43 -0300

    Primer commit de prueba

--- src/main/java/com/isw/app/config/Constants.java
+++ src/main/java/com/isw/app/config/Constants.java
@@ -4,7 +4,7 @@ package com.isw.app.config;
  * Application constants.
  */
 public final class Constants {
-
+	//comentario 
     // Regex for acceptable logins
     public static final String LOGIN_REGEX = "^(?>[a-zA-Z0-9!$&*+=?^_`{|}~.-]+@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*)|(?>[_.@A-Za-z0-9-]+)$";
 
